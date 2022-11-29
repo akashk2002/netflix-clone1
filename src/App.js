533f7481fff49867fdb7c0fb3b2e09c64b106919
akashk2002 <kanyadharaakash@gmail.com>
2022-11-29 14:26:03 +0530

    DAY 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+const App = () => {
+  const Name = 'Akash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <div>
+
+<h1 className="title" >The address {Name} </h1>
+<h1>This is a heading</h1>
+<p style={{fontSize:'10rem'}}>This is a paragraph.</p>
+</div>
+
+};
+
 
 export default App;
